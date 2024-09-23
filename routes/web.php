01ad18f99318bce3cfd871d82e1ae4b07680b1dd
Kukoh Fatchu Rahman <kukohfr@gmail.com>
2024-09-23 19:44:30 +0700

    mempersiapkan file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+ 
+Route::get('login', [AuthController::class, 'login']);
