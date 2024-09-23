db3909e7298dfc1be36c9e05df3083239c24b5e1
Kukoh Fatchu Rahman <kukohfr@gmail.com>
2024-09-24 00:54:48 +0700

    edit fillable column di model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,9 +17,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
+        'phone',
+        'address',
     ];
 
     /**
