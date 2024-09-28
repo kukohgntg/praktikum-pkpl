8f746c790d9104a58c4b4d20b403bd96bae96691
Kukoh Fatchu Rahman <kukohfr@gmail.com>
2024-09-29 02:04:46 +0700

    add route on bootstrap app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,23 @@
 <?php
 
+use App\Http\Middleware\OnlyAdmin;
+use App\Http\Middleware\OnlyGuest;
+use App\Http\Middleware\OnlyClient;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
+        // health: '/up',
+        health: '/status',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias(['onlyadmin', OnlyAdmin::class]);
+        $middleware->alias(['onlyclient', OnlyClient::class]);
+        $middleware->alias(['onlyguest', OnlyGuest::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
