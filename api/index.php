f45262f956530b92ae598bf33a2ea0d13c3eaeda
Kukoh Fatchu Rahman <kukohfr@gmail.com>
2024-09-29 08:45:19 +0700

    require __DIR__ . '/../bootstrap/app.php';

--- api/index.php
+++ api/index.php
@@ -2,12 +2,4 @@
 
 require __DIR__ . '/../public/index.php';
 require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
- 
-// Run the application
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-$response->send();
-$kernel->terminate($request, $response);
+require __DIR__ . '/../bootstrap/app.php';
